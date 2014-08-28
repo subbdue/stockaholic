d6d2417ff54a73b4ab4a09a54dcb95a673e39d95
subbdue <g.subramani@gmail.com>
2014-08-27 20:38:29 -0700

    routine that is loading periodically with latest stock prices

---
+++ update_stocks.js
@@ -0,0 +1,14 @@
+  function updateStockPrices() {
+      var slist = {"AAPL":[101.35, "+1.5%", "success"], "CSCO":[23.7, "-0.75%", "primary"], "PANW":[84.35, "+0.75", "mild-success"]};
+      for(var ii in slist) {
+        if(slist.hasOwnProperty(ii)){
+            var symbl = '#'+ii;
+            var newPrice = slist[ii][0]+"<br>"+slist[ii][1];
+            var newClass = "btn btn-"+slist[ii][2];
+            $(symbl).children("a").children("p.price").html(newPrice);
+            /* Change Class http://stackoverflow.com/questions/3452778/jquery-change-class-name */
+            $(symbl).children("a").attr("class", newClass);
+        }
+      }
+      /*var t = setTimeout(function(){updateStockPrices()},5000);*/
+  }
