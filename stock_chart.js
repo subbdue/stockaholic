d81a2d8b0d2ff6b330f3eaab5c61933d8dc28723
subbdue <g.subramani@gmail.com>
2014-08-31 23:28:11 -0700

    test: charts

--- stock_chart.js
+++ stock_chart.js
@@ -1,6 +1,30 @@
-  function drawStockTable() {
-      var slist = '<div class="row-res" id="SYF"><a class="btn btn-warning" href="finance.yahoo.com/q?s=PANW" target="_blank"><p class="sybl">SYF</p><p class="price">84.11<br>+.05%</p></a></div>' +
-          '<div class="row-res" id="BOA"><a class="btn btn-warning" href="finance.yahoo.com/q?s=PANW" target="_blank"><p class="sybl">BOA</p><p class="price">84.11<br>+.05%</p></a></div>';
-      var symbl = '#sector-fin';
-      $(symbl).html(slist);
-  }
+//var sectors = ['bi', 'cg', 'nd', 'services', 'energy', 'finance', 'health', 'misc', 'utils', 'tech', 'trans'];
+var slist = {};
+slist['bi'] = {
+   "AAPL":[101.35, "+0.15%", "mild-success"],
+   "CSCO":[23.7, "-8.70%", "danger"],
+   "PANW":[82.35, "-0.05%", "primary"],
+   "GPRO":[47.10, "+10.55%", "success"],
+};
+slist['energy'] = {
+   "AXYZ":[101.35, "+0.15%", "mild-success"],
+   "CXYZ":[23.7, "-8.70%", "danger"],
+   "PXYZ":[82.35, "-0.05%", "primary"],
+   "GXYZ":[47.10, "+10.55%", "success"],
+};
+
+function drawStockTable() {
+        for(var sector in slist) {
+            var slist_html = '';
+            if(slist.hasOwnProperty(sector)) {
+                for(var stock in slist[sector]) {
+                    if(slist[sector].hasOwnProperty(stock)) {
+                        var ss = slist[sector][stock];
+                        slist_html += '<div class="row-res" id="'+stock+'"><a class="btn btn-'+ss[2]+'" href="finance.yahoo.com/q?s='+stock+'" target="_blank"><p class="sybl">'+stock+'</p><p class="price">'+ss[0]+'<br>'+ss[1]+'</p></a></div>'
+                    }
+                }
+            }
+            var symbl = '#sector-'+sector;
+            $(symbl).html(slist_html);
+        }
+}
